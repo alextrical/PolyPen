b964d5e35822b57fb7b5d7810359361e6070d4ca
alextrical <35117191+alextrical@users.noreply.github.com>
2023-06-03 12:12:05 +0100

    Setup of core dependencies for the GUI

---
+++ PolyPen/src/PolyPenCore.cpp
@@ -0,0 +1,17 @@
+#include "src/generated/PolyPen_menu.h"
+
+void setup() {
+    Wire.begin();
+    setupMenu();
+
+}
+
+void loop() {
+    taskManager.runLoop();
+
+}
+
+
+void CALLBACK_FUNCTION onStart(int id) {
+    // TODO - your menu change code
+}
